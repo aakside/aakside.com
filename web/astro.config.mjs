bc437e38b0ba7e88dcc97d66b1437e89a86dac5e
Alvin Ali Khaled <aakside@gmail.com>
2025-12-10 01:00:08 -0500

    Change default typeface to Myriad Pro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,15 +2,20 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		fonts: [{
+			provider: fontProviders.adobe({ id: "oft2wtu" }),
+			name: "Myriad Pro",
+			cssVariable: "--font-myriad-pro"
+		}]
+	},
   integrations: [mdx(), sitemap()],
   site: "https://aakside.com",
-
   vite: {
     plugins: [tailwindcss()],
   },
