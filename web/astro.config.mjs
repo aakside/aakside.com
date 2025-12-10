35d209262031d84d3e0ed66d2396a846730ee2fc
Alvin Ali Khaled <aakside@gmail.com>
2025-12-09 23:58:57 -0500

    Change default typeface

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), sitemap()],
-	site: "https://aakside.com",
+  integrations: [mdx(), sitemap()],
+  site: "https://aakside.com",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
