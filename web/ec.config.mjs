1c6e90a9d2523bc779e64aa792d58a4614c9a550
Alvin Ali Khaled <aakside@gmail.com>
2026-03-12 18:23:42 -0400

    Add search

--- web/ec.config.mjs
+++ web/ec.config.mjs
@@ -2,6 +2,9 @@ import { defineEcConfig } from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 
 export default defineEcConfig({
+  frames: {
+    showCopyToClipboardButton: false,
+  },
   plugins: [pluginCollapsibleSections()],
   themes: ["ayu-mirage"],
 });
