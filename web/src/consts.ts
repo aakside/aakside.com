00098b5314222add4c61d94f4770f12b82f39269
Alvin Ali Khaled <aakside@gmail.com>
2026-08-21 16:59:57 -0400

    Add JSON-LD

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,4 +1,23 @@
 export const GITHUB_REPO_URL = "https://github.com/aakside/aakside.com";
 export const SITE_AUTHOR = "Alvin Ali Khaled";
-export const SITE_TITLE = "aakside";
 export const SITE_DESCRIPTION = "The personal website of Alvin Ali Khaled";
+export const SITE_TITLE = "aakside";
+export const SITE_SAME_AS = [
+  "https://bandcamp.com/aakside",
+  "https://en.wikipedia.org/wiki/User:AAK",
+  "https://flickr.com/photos/aakside/",
+  "https://github.com/aakside/",
+  "https://gitlab.com/aakside",
+  "https://listenbrainz.org/user/aakside/",
+  "https://mastodon.social/@aakside",
+  "https://soundcloud.com/aakside",
+  "https://stackexchange.com/users/1021716/aakside",
+  "https://super-hell-fighter.xanga.com/",
+  "https://www.deviantart.com/aakside",
+  "https://www.instagram.com/aakside",
+  "https://www.last.fm/user/AAK",
+  "https://www.patreon.com/aakside",
+  "https://www.reddit.com/user/aakside/",
+  "https://www.youtube.com/aakside",
+  "https://x.com/aakside",
+];
